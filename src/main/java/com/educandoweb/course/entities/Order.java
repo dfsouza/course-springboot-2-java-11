d2be04ad28c335fe1bcde34045e6ef690c8db011
Daniel Souza <danielfsouza@gmail.com>
2021-06-07 20:35:22 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -30,14 +30,17 @@ public class Order implements Serializable{
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	private Integer orderStatus;
+	
 	public Order () {
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -81,6 +84,23 @@ public class Order implements Serializable{
 	 */
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+
+	/**
+	 * @return the orderStatus
+	 */
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	/**
+	 * @param orderStatus the orderStatus to set
+	 */
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+		   this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
