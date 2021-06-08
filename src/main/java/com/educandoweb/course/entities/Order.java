709e4e53ca2d0e04855757f0d3499f44f1693f75
Daniel Souza <danielfsouza@gmail.com>
2021-06-08 00:09:16 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -115,6 +115,15 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+	public Double getTotal() {
+		
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+
 
 	/**
 	 * @return the orderStatus
